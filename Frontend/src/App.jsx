e5f507d635ef148afcb6c1debe8ac8b88112694c
satyaprakash2002 <sarangisatya2002@gmail.com>
2024-04-03 01:28:38 +0530

    phase 1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './customComponents/Button'
 
 function App() {
 
+  const handleClick = () => {
+    console.log("hello world")
+  }
+
   return (
     <>
-      
+      <Button>Click Here</Button>
     </>
-  )
+  );
 }
 
 export default App
--- Frontend/src/customComponents/Button.jsx
+++ Frontend/src/customComponents/Button.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { cva } from "class-variance-authority";
 
-const Button = (props) => {
-  return (
-    <div>Button</div>
-  )
-}
+const button = cva("button", {
+  variants: {
+    intent: {
+      primary: [
+        "bg-blue-500",
+        "text-white",
+        "border-transparent",
+        "hover:bg-blue-600",
+      ],
+      secondary: [
+        "bg-white",
+        "text-gray-800",
+        "border-gray-400",
+        "hover:bg-gray-100",
+      ],
+    },
+    size: {
+      small: ["text-sm", "py-1", "px-2"],
+      medium: ["text-base", "py-2", "px-4"],
+    },
+  },
+  compoundVariants: [
+    {
+      intent: "primary",
+      size: "medium",
+      className: "uppercase",
+    },
+  ],
+  defaultVariants: {
+    intent: "primary",
+    size: "medium",
+  },
+});
 
-export default Button
+const Button = ({ intent, size, className, ...props }) => (
+  <button className={button({ intent, size, className })} {...props} />
+);
+
+export default Button;
