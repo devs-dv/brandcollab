a8cd82a1d140041a3b0ea153ea43433b9ad4fb31
satyaprakash2002 <sarangisatya2002@gmail.com>
2024-04-05 19:45:33 +0530

    satya

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './customComponents/Button'
+import Text from './customComponents/Text';
 
 function App() {
 
   return (
     <>
-      
+      <Text weight="bold" className="text-xl">Hello User</Text>
     </>
   );
 }
--- Frontend/src/customComponents/Text.jsx
+++ Frontend/src/customComponents/Text.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
 import { cva } from "class-variance-authority";
+import "../App.css"
 
-const Text = () => {
-  return (
-    <div>Text</div>
-  )
-}
+const text = cva("div", {
+  variants: {
+    color: {
+      primary: ["text-blue-500"],
+      secondary: ["text-gray-800"],
+    },
+    size: {
+      small: ["text-sm"],
+      medium: ["text-base"],
+      large: ["text-lg"],
+    },
+    weight: {
+      regular: ["font-normal barlowFont"],
+      bold: ["font-bold barlowFont"],
+    },
+  },
+  defaultVariants: {
+    color: "secondary",
+    size: "medium",
+    weight: "regular",
+  },
+});
 
-export default Text
+const Text = ({ color, size, weight, className, ...props }) => (
+  <p className={text({ color, size, weight, className })} {...props}>
+  </p>
+);
+
+export default Text;
