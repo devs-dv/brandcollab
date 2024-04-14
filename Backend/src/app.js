f3ee526e358ac97d40472c4429d7dd8b17fe7f15
Rajan Kumar <raorajan9576@gmail.com>
2024-04-14 12:07:45 +0530

    chat model created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -35,73 +35,8 @@ app.use(cors(corsOptions));
 Connection();
 app.use("/api/v1", userRouter);
 
-const users = {};
-
 app.get("/", (req, res) => {
   res.send("Server is Running! 🚀");
-});
-
-
-const emailToSocketIdMap = new Map();
-const socketidToEmailMap = new Map();
-
-io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id);
-
-  //for video chatting
-  socket.on("room:join", (data) => {
-    const { email, room } = data;
-    emailToSocketIdMap.set(email, socket.id);
-    socketidToEmailMap.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
-  });
-
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incomming:call", { from: socket.id, offer });
-  });
-
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
-  });
-
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-  });
-
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-  });
-
-  //for text chatting
-
-  socket.on("joined", ({ user }) => {
-    users[socket.id] = user;
-    console.log(`${user} has joined`);
-    socket.broadcast.emit("userJoined", {
-      user: "Admin",
-      message: `${users[socket.id]} has joined`,
-    });
-    socket.emit("welcome", {
-      user: "Admin",
-      message: `Welcome to the chat, ${users[socket.id]}`,
-    }); 
-  });
-
-  socket.on("message", ({ message, id }) => {
-    io.emit("sendMessage", { user: users[id], message, id });
-  });
-
-  socket.on("disconnect", ({ user }) => {
-    socket.broadcast.emit("leave", {
-      user: "Admin",
-      message: `${users[socket.id]} has left`,
-    });
-    console.log(`${user} has left`);
-  });
 });
 
 export default runServer;
