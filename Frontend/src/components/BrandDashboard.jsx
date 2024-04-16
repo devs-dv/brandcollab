cfbacff91171effdcd9768d93ec46b03a0cc021b
satyaprakash2002 <sarangisatya2002@gmail.com>
2024-04-16 11:02:48 +0530

    satya

--- Frontend/src/components/BrandDashboard.jsx
+++ Frontend/src/components/BrandDashboard.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import TopNav from './navigation/TopNav';
+import SideNav from './navigation/SideNav';
+import InfDashAbout from '../pages/InfDash Cards/InfDashAbout';
+import InfDashSocials from '../pages/InfDash Cards/InfDashSocials';
+import BranDashAbout from '../pages/BranDash Cards/BranDashAbout';
 
 const BrandDashboard = () => {
   return (
-    <div>BrandDashboard</div>
-  )
+    <>
+      <div className="bg-gray-100 ">
+        <TopNav />
+        <div className="flex flex-col md:flex-row">
+          <div className=" md:sticky md:top-0 md:left-0 md:h-screen bg-gray-800 text-white">
+            <SideNav />
+          </div>
+          <div className="flex-1">
+            <BranDashAbout/>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BrandDashboard
