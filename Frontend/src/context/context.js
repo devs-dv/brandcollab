cb8a289642b455d326dd1f6d15991badb6641ac5
Rajan Kumar <raorajan9576@gmail.com>
2024-04-29 21:01:57 +0530

    changes in context

--- Frontend/src/context/context.js
+++ Frontend/src/context/context.js
@@ -1,3 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const valContext = createContext();
+export const valContext = createContext(null);
+
+export const ContextProvider = (props) => {
+  const [data, setData] = useState();
+  return (
+    <valContext.Provider value={{ data, setData }}>
+      {props.children}
+    </valContext.Provider>
+  );
+};
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ContextProvider } from "./context/context.js";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
         <App />
-    </BrowserRouter>
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>
 );
