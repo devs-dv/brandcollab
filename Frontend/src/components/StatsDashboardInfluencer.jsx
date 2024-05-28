4f755624ebc2c583f6f6f5fe5eb7821f52878d8d
Divyanshu Verma <divyanshudverma@gmail.com>
2024-05-28 17:29:16 +0530

    DV - main stats added

--- Frontend/src/components/StatsDashboardInfluencer.jsx
+++ Frontend/src/components/StatsDashboardInfluencer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const StatsDashboardInfluencer = () => {
   return (
     <div>
+      {/* Main Stats Starts here  */}
       <div className="px-6 pt-6 2xl:container">
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           <div className="md:col-span-2 lg:col-span-1">
@@ -776,6 +777,8 @@ const StatsDashboardInfluencer = () => {
           </div>
         </div>
       </div>
+
+      {/* Main Stats ends here  */}
     </div>
   );
 };
