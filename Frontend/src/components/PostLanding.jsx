575d2f99e10867968414e3e84605a7b39dda961a
Divyanshu Verma <divyanshudverma@gmail.com>
2024-05-30 23:55:34 +0530

    DV

--- Frontend/src/components/PostLanding.jsx
+++ Frontend/src/components/PostLanding.jsx
@@ -96,14 +96,14 @@ const PostLanding = () => {
 
   return (
     <div>
-      <SideNav/>
-      <main className="content ml-12 transition-all ease-in-out duration-500 ">
+      {/* <SideNav/>
+      {/* <main className="content ml-12 transition-all ease-in-out duration-500 ">
         <div className="h-16" />
         {/* Your main content starts here */}
         {renderMainContent()}
 
         {/* Your main content ends here */}
-      </main>
+      {/* </main> */} 
     </div>
   );
 };
