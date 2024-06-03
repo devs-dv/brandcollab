a443bc9da4b14e22b0ec7e92c9553b01f8ec1136
satyaprakash2002 <sarangisatya2002@gmail.com>
2024-06-03 22:26:01 +0530

    satya

--- Frontend/src/components/navigation/BrandSideNav.jsx
+++ Frontend/src/components/navigation/BrandSideNav.jsx
@@ -247,7 +247,7 @@ const TopNav = () => {
               </div>
             </Link>
 
-            <Link to="/postlanding/collabOngoing">
+            <Link to="/BrandPostLanding/BrandCollabOngoing">
               <div
                 onClick={() => handleOptionSelect("OngoingCollab")}
                 className="hover:ml-4 w-full text-white hover:text-purple-500 dark:hover:text-blue-500 bg-[#1E293B] p-2 pl-8 rounded-full transform ease-in-out duration-300 flex flex-row items-center space-x-3"
@@ -270,7 +270,7 @@ const TopNav = () => {
               </div>
             </Link>
 
-            <Link to="/postlanding/collabComplete">
+            <Link to="/BrandPostLanding/BrandCollabComplete">
               <div
                 onClick={() => handleOptionSelect("CompletedCollab")}
                 className="hover:ml-4 w-full text-white hover:text-purple-500 dark:hover:text-blue-500 bg-[#1E293B] p-2 pl-8 rounded-full transform ease-in-out duration-300 flex flex-row items-center space-x-3"
@@ -389,7 +389,7 @@ const TopNav = () => {
                 </svg>
               </div>
             </Link>
-            <Link to="/postlanding/collabOngoing">
+            <Link to="/BrandPostLanding/BrandCollabOngoing">
               <div
                 onClick={() => handleOptionSelect("OngoingCollab")}
                 className="hover:ml-4 justify-end pr-5 text-white hover:text-purple-500 dark:hover:text-blue-500 w-full bg-[#1E293B] p-3 rounded-full transform ease-in-out duration-300 flex"
@@ -411,7 +411,7 @@ const TopNav = () => {
               </div>
             </Link>
 
-            <Link to="/postlanding/collabComplete">
+            <Link to="/BrandPostLanding/BrandCollabComplete">
               <div
                 onClick={() => handleOptionSelect("CompletedCollab")}
                 className="hover:ml-4 justify-end pr-5 text-white hover:text-purple-500 dark:hover:text-blue-500 w-full bg-[#1E293B] p-3 rounded-full transform ease-in-out duration-300 flex"
