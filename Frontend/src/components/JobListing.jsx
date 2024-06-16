e0ca1ebc46e07b9399834387138c74b3e390a18d
Divyanshu Verma <divyanshudverma@gmail.com>
2024-06-16 16:08:10 +0530

    DV

--- Frontend/src/components/JobListing.jsx
+++ Frontend/src/components/JobListing.jsx
@@ -188,6 +188,7 @@ const JobListing = () => {
           </div>
         </div>
         {/* Your main content ends here */}
+        {/* here it will for commit and pull */}
       </main>
     </div>
   );
