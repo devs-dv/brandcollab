814d92083b1095c83d8d41c36bbee1c647252c75
satyaprakash2002 <sarangisatya2002@gmail.com>
2024-06-18 11:11:27 +0530

    satya

--- Frontend/src/components/BrandCollabOngoing.jsx
+++ Frontend/src/components/BrandCollabOngoing.jsx
@@ -19,9 +19,7 @@ const BrandCollabOngoing = () => {
                   </h1>
                   <p className="mb-6 max-w-[528px] text-xl md:mb-10">
                     {/* description of the brand  */}
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                    aliquam, purus sit amet luctus venenatis, lectus
-                    
+                    Your on going collborations with approved influencer !!!
                   </p>
                   <div className="flex items-center">
                     <a
@@ -31,13 +29,7 @@ const BrandCollabOngoing = () => {
                     >
                       Let's Talk
                     </a>
-                    <a
-                      href="#"
-                      className="flex max-w-full flex-row rounded-full border border-solid border-[#636262] px-6 py-4 font-bold"
-                    >
-                      <p className="">View Contract</p>
-                      {/* dummy contract report  */}
-                    </a>
+                    
                   </div>
                 </div>
                 <div>
@@ -53,7 +45,7 @@ const BrandCollabOngoing = () => {
                           />
                           <div className="py-2">
                             <h3 className="font-bold text-2xl text-gray-800 dark:text-white mb-1">
-                              Influencer Name 
+                              User One
                             </h3>
                             <div className="inline-flex text-gray-700 dark:text-gray-300 items-center">
                               <svg
@@ -69,17 +61,15 @@ const BrandCollabOngoing = () => {
                                   d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
                                 />
                               </svg>
-                              Location , NY
+                              India, Delhi, South Delhi
                             </div>
                           </div>
                         </div>
                         <div className="flex gap-2 px-2">
                           <button className="flex-1 rounded-full bg-blue-600 dark:bg-blue-800 text-white dark:text-white antialiased font-bold hover:bg-blue-800 dark:hover:bg-blue-900 px-4 py-2">
-                            Followers 
+                            Followers : 50000
                           </button>
-                          <button className="flex-1 rounded-full border-2 border-gray-400 dark:border-gray-700 font-semibold text-black dark:text-white px-4 py-2">
-                           following 
-                          </button>
+                          
                         </div>
                       </div>
                       <div className="px-4 py-4">
